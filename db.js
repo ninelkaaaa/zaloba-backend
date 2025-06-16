9f7aeaddce510053ffd9079b361101ae99c356c3
ninelkaaaa <ninelsafarova55@gmail.com>
2025-06-16 18:43:17 +0500

    up

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'zaloba_db_user',         // смотри в Render → DATABASE → Internal Database URL
-  host: 'dpg-d1818i8gjchc73f92gkg-a',
+  host: 'dpg-d1818i8gjchc73f92gkg-a.oregon-postgres.render.com',
   database: 'zaloba-db',    // название твоей базы
   password: 'KLdBeUAj1ruuOX0rRdd3nSOmPMYkC9q3',
   port: 5432,
