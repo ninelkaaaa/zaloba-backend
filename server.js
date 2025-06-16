cd53958b22221e0e65364e5121a60f65c134cb1a
ninelkaaaa <ninelsafarova55@gmail.com>
2025-06-16 18:35:21 +0500

    update db config for render

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 
+const cors = require('cors');
 const app = express();
 const routes = require('./routes');
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://zaloba.vercel.app',
+    credentials: true
+  }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(routes);
